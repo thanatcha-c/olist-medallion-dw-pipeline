e258f131b7b9a7af00e0ee41525b566e88ba903c
thanatcha-c@hotmail.com <thanatcha-c@hotmail.com>
2026-09-23 19:21:04 +0000

    update gold orders model to include order items aggregation

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,14 +1,23 @@
-with order_payments_agg as (
+with order_items_agg as (
     select
         order_id,
-        count(payment_sequential) as payment_count,
+        count(*) as item_count,
+        sum(price) as total_sales,
+        sum(freight_value) as total_freight_value
+    from  {{ source("silver","order_items")}} 
+    group by order_id
+),
+
+order_payments_agg as (
+    select
+        order_id,
+        count(*) as payment_count,
         sum(payment_value) as total_payment_value
-    from {{ source("silver","order_payments")}}
+    from  {{ source("silver","order_payments")}} 
     group by order_id
 )
 
 select
-    row_number() over(order by o.order_id) as id,
     o.order_id,
     o.customer_id,
     o.order_status as status,
@@ -19,8 +28,12 @@ select
     o.order_estimated_delivery_ts as estimated_delivery_at,
     o.delivery_days,
     o.is_delayed,
+    coalesce(i.item_count, 0) as item_count,
+    coalesce(i.total_sales, 0) as total_sales,
+    coalesce(i.total_freight_value, 0) as total_freight_value,
     coalesce(p.payment_count, 0) as payment_count,
-    coalesce(p.total_payment_value, 0.0) as total_payment_value
+    coalesce(p.total_payment_value, 0) as total_payment_value
 
 from {{ source("silver","orders")}} o
+left join order_items_agg i on o.order_id = i.order_id
 left join order_payments_agg p on o.order_id = p.order_id
