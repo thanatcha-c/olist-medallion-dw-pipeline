512002388cbb18326b5299609cc5e9c9cc2de3c5
thanatcha-c@hotmail.com <thanatcha-c@hotmail.com>
2026-09-24 08:22:37 +0000

    Add surrogate key

--- models/marts/fct_order_payments.sql
+++ models/marts/fct_order_payments.sql
@@ -1,5 +1,5 @@
 select
-    row_number() over(order by order_id, payment_sequential) as id
+    row_number() over(order by order_id, payment_sequential) as id,
     order_id,
     payment_sequential as payment_seq,
     payment_type,
